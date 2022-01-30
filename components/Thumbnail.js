c7bc27092ff3e84b69a8e19f84957c76a156a0f9
Susanta <inbox.susanta@gmail.com>
2022-01-30 18:18:09 +0530

    Style the result component

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,12 +1,16 @@
 import { ThumbUpIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import Results from './Results';
+import { forwardRef } from 'react';
 
-function Thumbnail({ result }) {
+const Thumbnail = forwardRef(({ result }, ref) => {
   // console.log(result);
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform  sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform  sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -31,6 +35,6 @@ function Thumbnail({ result }) {
       </div>
     </div>
   );
-}
+});
 
 export default Thumbnail;
